0ecdfb703a38b671b4f550502bed6bb9128fe9cb
LuisColumnaDeveloper <lmcolumna@gmail.com>
2019-02-20 22:38:18 -0600

    SEND EMERGENCY ** envia emergencias y trackeo, configuración preferencias

--- app/src/main/java/mx/gob/cdmx/ssc/mpt/Fragments/FragmentToken.java
+++ app/src/main/java/mx/gob/cdmx/ssc/mpt/Fragments/FragmentToken.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -33,6 +35,8 @@ public class FragmentToken extends Fragment {
 
     Button btnScan;
 
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,6 +56,20 @@ public class FragmentToken extends Fragment {
         btnScan = (Button)view.findViewById(R.id.btnScan);
 
         tvToken.setText(TOKEN);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager;
+                FragmentTransaction fragmentTransaction;
+                fragmentManager = getFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+
+                FragmentScan fragmentScan= new FragmentScan();
+                fragmentTransaction.replace(R.id.fragment, fragmentScan);
+                fragmentTransaction.commit();
+            }
+        });
 
         return view;
     }
